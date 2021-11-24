8a3bf63732637e0a4b349044078e2cdc323ba9ff
Marco Cutecchia <marco.cutecchia@outlook.it>
2021-11-24 16:39:29 +0100

    CLI: Add a simple test main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
 mod lexer;
+mod parser;
 
-fn main() {}
+fn main() {
+    let source = r#"
+    
+    fn factorial(x: int) -> int {
+        var result = 1;
+        while (x > 0) {
+            result = result * x;
+            x = x - 1;
+        }
+
+        return result;
+    }
+
+    fn main() -> void {
+
+    }
+    "#;
+
+    let tokens = lexer::tokenize(source);
+    if let Err(errors) = tokens {
+        for lexer_error in errors {
+            match lexer_error.error {
+                lexer::ErrorType::UnrecognizedToken(bad_token) => {
+                    eprintln!(
+                        "Syntax Error: Unrecognized token '{}' at line: {}, column: {}",
+                        bad_token, lexer_error.line, lexer_error.column
+                    );
+                }
+            }
+        }
+        return;
+    }
+    let tokens = tokens.unwrap();
+    println!("{:?}", tokens);
+
+    let ast = parser::parse(&tokens);
+    if let Err(error) = ast {
+        eprintln!("Parse Error: {}", error);
+        return;
+    }
+    let ast = ast.unwrap();
+    println!("{:?}", ast);
+}
