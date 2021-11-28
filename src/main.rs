60c7a2b5bbd8ee4898bf48a8f37928c487498294
Marco Cutecchia <marco.cutecchia@outlook.it>
2021-11-28 12:40:46 +0100

    Main: Move error printing in their own functions

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,28 @@
+use interpreter::lexer::LexerError;
+
 mod interpreter;
+
+fn print_lexer_errors(errors: &Vec<LexerError>) {
+    for lexer_error in errors {
+        match lexer_error.error {
+            interpreter::lexer::ErrorType::UnrecognizedToken(bad_token) => {
+                eprintln!(
+                    "Syntax Error: Unrecognized token '{}' at line: {}, column: {}",
+                    bad_token, lexer_error.line, lexer_error.column
+                );
+            }
+        }
+    }
+}
+
+fn print_parsing_errors(errors: &str) {
+    eprintln!("Parse Error: {}", errors);
+}
 
 fn main() {
     let source = r#"
     fn factorial(x: int) -> int {
-        var result = 1;
+        var result: int = 1;
         while (x > 0) {
             result *= x;
             x -= 1;
@@ -19,26 +38,17 @@ fn main() {
 
     let tokens = interpreter::tokenize(source);
     if let Err(errors) = tokens {
-        for lexer_error in errors {
-            match lexer_error.error {
-                interpreter::lexer::ErrorType::UnrecognizedToken(bad_token) => {
-                    eprintln!(
-                        "Syntax Error: Unrecognized token '{}' at line: {}, column: {}",
-                        bad_token, lexer_error.line, lexer_error.column
-                    );
-                }
-            }
-        }
+        print_lexer_errors(&errors);
         return;
     }
     let tokens = tokens.unwrap();
     println!("{:#?}", tokens);
 
-    let ast = interpreter::parse(&tokens);
-    if let Err(error) = ast {
-        eprintln!("Parse Error: {}", error);
+    let parse_tree = interpreter::parse(&tokens);
+    if let Err(errors) = parse_tree {
+        print_parsing_errors(errors);
         return;
     }
-    let ast = ast.unwrap();
-    println!("{:#?}", ast);
+    let parse_tree = parse_tree.unwrap();
+    println!("{:#?}", parse_tree);
 }
