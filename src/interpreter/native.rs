e8d55023bd8f0e564410d0d905e4cf44f05ea01a
Marco Cutecchia <marco.cutecchia@outlook.it>
2021-12-06 13:19:33 +0100

    Eval: Add 'len' to native functions

--- src/interpreter/native.rs
+++ src/interpreter/native.rs
@@ -2,11 +2,25 @@ use crate::interpreter::{ast::ImmediateValue, environment::Env, typechecking::Ty
 use std::rc::Rc;
 
 pub fn fill_env_with_native_functions(env: &Rc<Env<ImmediateValue>>) {
+    // I/O
     env.declare(
         "print",
         ImmediateValue::NativeFunction(
             Type::Closure(vec![Type::Array(Box::new(Type::Any))], Box::new(Type::Void)),
             native_print,
+        ),
+        true,
+    );
+
+    // Array operations
+    env.declare(
+        "len",
+        ImmediateValue::NativeFunction(
+            Type::Closure(
+                vec![Type::Array(Box::new(Type::Any))],
+                Box::new(Type::Integer),
+            ),
+            native_array_len,
         ),
         true,
     );
@@ -37,4 +51,13 @@ fn native_print(args: Vec<ImmediateValue>) -> ImmediateValue {
         print_immediate_value(&arg);
     }
     ImmediateValue::Void
+}
+
+fn native_array_len(args: Vec<ImmediateValue>) -> ImmediateValue {
+    match args.get(0) {
+        Some(ImmediateValue::Array(_, array)) => {
+            ImmediateValue::Integer(array.borrow().len() as i64)
+        },
+        _ => panic!("Typechecker failed! Native function 'len' was called with an argument that is not an array")
+    }
 }
